6037794fd0c0c0533ac586c4e140a20035b0c6d5
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-08-26 06:34:54 -0400

    commit tree e97566dc82b095516e7795ebde0a63aaf32bb3c6

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// anon function using map method on tutorial array
+// use split on the elements (with space between '' to denote empty char) to enable another map method
+// run 2nd map method with new element run another cb function to uppercase at char1 and slice original lowercase
+// .join so the array become sentences again
+
+// const titleCased = () => tutorials.map(ele => ele.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '))
+
+function titleCased() {
+  return tutorials.map(word => word.split(' ').map(letters => letters.charAt(0).toUpperCase() + letters.slice(1)).join(' '))
 }
